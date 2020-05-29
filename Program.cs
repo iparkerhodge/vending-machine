973b5ecf10040013a932fcd62aecaf32ec1196d9
Parker Hodge <iparkerhodge@gmail.com>
2020-05-29 15:01:42 -0500

    completed exercise

--- Program.cs
+++ Program.cs
@@ -1,147 +1,136 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
-namespace Vending
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Welcome to the Vending Machine!");
+namespace Vending {
+    class Program {
+        static void Main (string[] args) {
+            Console.WriteLine ("Welcome to the Vending Machine!");
 
-            VendingMachine machine = new VendingMachine();
+            VendingMachine machine = new VendingMachine ();
 
-            while (true)
-            {
-                Console.WriteLine();
+            while (true) {
+                Console.WriteLine ();
 
-                int selection = Menu();
-                switch (selection)
-                {
+                int selection = Menu ();
+                switch (selection) {
                     case 1:
-                        List<Product> allProudcts = machine.GetAll();
-                        Console.WriteLine("All Products:");
-                        foreach (Product p in allProudcts)
-                        {
-                            p.Display();
+                        List<Product> allProudcts = machine.GetAll ();
+                        Console.WriteLine ("All Products:");
+                        foreach (Product p in allProudcts) {
+                            p.Display ();
                         }
-                        Console.WriteLine($"Total Count: {allProudcts.Count}");
+                        Console.WriteLine ($"Total Count: {allProudcts.Count}");
                         break;
                     case 2:
-                        List<string> names = machine.GetProductNames();
-                        Console.WriteLine("All Product Names:");
-                        foreach (string name in names)
-                        {
-                            Console.WriteLine($" {name}");
+                        List<string> names = machine.GetProductNames ();
+                        Console.WriteLine ("All Product Names:");
+                        foreach (string name in names) {
+                            Console.WriteLine ($" {name}");
                         }
                         break;
                     case 3:
-                        Console.Write("Name> ");
-                        string nameCriteria = Console.ReadLine();
-                        List<Product> productsByName = machine.SearchByName(nameCriteria);
+                        Console.Write ("Name> ");
+                        string nameCriteria = Console.ReadLine ();
+                        List<Product> productsByName = machine.SearchByName (nameCriteria);
 
-                        Console.WriteLine("Results:");
-                        foreach (Product p in productsByName)
-                        {
-                            Console.Write(" ");
-                            p.Display();
+                        if (productsByName.Any ()) {
+                            Console.WriteLine ("Results:");
+                            foreach (Product p in productsByName) {
+                                Console.Write (" ");
+                                p.Display ();
+                            }
+                        } else {
+                            Console.WriteLine ("Product not found.");
                         }
                         break;
                     case 4:
-                        Console.Write("Min Price> ");
-                        string strMin = Console.ReadLine();
-                        double min = double.Parse(strMin);
-                        Console.Write("Max Price> ");
-                        string strMax = Console.ReadLine();
-                        double max = double.Parse(strMax);
+                        Console.Write ("Min Price> ");
+                        string strMin = Console.ReadLine ();
+                        double min = double.Parse (strMin);
+                        Console.Write ("Max Price> ");
+                        string strMax = Console.ReadLine ();
+                        double max = double.Parse (strMax);
 
-                        List<Product> productsByPrice = machine.SearchByPrice(min, max);
+                        List<Product> productsByPrice = machine.SearchByPrice (min, max);
 
-                        Console.WriteLine("Results:");
-                        foreach (Product p in productsByPrice)
-                        {
-                            Console.Write(" ");
-                            p.Display();
+                        Console.WriteLine ("Results:");
+                        foreach (Product p in productsByPrice) {
+                            Console.Write (" ");
+                            p.Display ();
                         }
                         break;
                     case 5:
-                        Product cheapest = machine.GetCheapest();
-                        cheapest.Display();
+                        Product cheapest = machine.GetCheapest ();
+                        cheapest.Display ();
                         break;
                     case 6:
-                        Product expensive = machine.GetMostExpensive();
-                        expensive.Display();
+                        Product expensive = machine.GetMostExpensive ();
+                        expensive.Display ();
                         break;
                     case 7:
-                        Console.Write("Product ID> ");
-                        int productId = int.Parse(Console.ReadLine());
-                        Product productToPurchase = machine.GetById(productId);
-                        if (productToPurchase != null)
-                        {
-                            machine.RemoveProduct(productToPurchase);
+                        Console.Write ("Product ID> ");
+                        int productId = int.Parse (Console.ReadLine ());
+                        Product productToPurchase = machine.GetById (productId);
+                        if (productToPurchase != null) {
+                            machine.RemoveProduct (productToPurchase);
                         }
                         break;
                     case 8:
-                        Console.WriteLine("Enter Product Details");
+                        Console.WriteLine ("Enter Product Details");
 
-                        Console.Write("ID> ");
-                        int newId = int.Parse(Console.ReadLine());
+                        Console.Write ("ID> ");
+                        int newId = int.Parse (Console.ReadLine ());
 
-                        Console.Write("Name> ");
-                        string newName = Console.ReadLine();
+                        Console.Write ("Name> ");
+                        string newName = Console.ReadLine ();
 
-                        Console.Write("Price> ");
-                        double newPrice = double.Parse(Console.ReadLine());
+                        Console.Write ("Price> ");
+                        double newPrice = double.Parse (Console.ReadLine ());
 
-                        Product newProduct = new Product()
-                        {
+                        Product newProduct = new Product () {
                             Id = newId,
                             Name = newName,
                             Price = newPrice,
                         };
-                        machine.AddProduct(newProduct);
+                        machine.AddProduct (newProduct);
                         break;
                     case 9:
-                        Console.WriteLine($"Total Value: {machine.TotalValue}");
+                        Console.WriteLine ($"Total Value: {machine.TotalValue}");
                         break;
-                    case 0: 
-                        Console.WriteLine("Goodbye");
+                    case 0:
+                        Console.WriteLine ("Goodbye");
                         return;
                     default:
-                        throw new Exception("Something went wrong...invalid selection");
+                        throw new Exception ("Something went wrong...invalid selection");
                 }
             }
         }
 
-        static int Menu()
-        {
+        static int Menu () {
             int selection = -1;
 
-            while (selection < 0 || selection > 9)
-            {
-                Console.WriteLine("Main Menu");
-                Console.WriteLine(" 1) View All (ordered by price)");
-                Console.WriteLine(" 2) View All Names (ordered alphabetically)");
-                Console.WriteLine(" 3) Search by Name");
-                Console.WriteLine(" 4) Search by Price");
-                Console.WriteLine(" 5) Find Cheapest");
-                Console.WriteLine(" 6) Find Most Expensive");
-                Console.WriteLine(" 7) Purchase Product");
-                Console.WriteLine(" 8) Add Product");
-                Console.WriteLine(" 9) Total Machine Value");
-                Console.WriteLine(" 0) Exit");
+            while (selection < 0 || selection > 9) {
+                Console.WriteLine ("Main Menu");
+                Console.WriteLine (" 1) View All (ordered by price)");
+                Console.WriteLine (" 2) View All Names (ordered alphabetically)");
+                Console.WriteLine (" 3) Search by Name");
+                Console.WriteLine (" 4) Search by Price");
+                Console.WriteLine (" 5) Find Cheapest");
+                Console.WriteLine (" 6) Find Most Expensive");
+                Console.WriteLine (" 7) Purchase Product");
+                Console.WriteLine (" 8) Add Product");
+                Console.WriteLine (" 9) Total Machine Value");
+                Console.WriteLine (" 0) Exit");
 
-                Console.Write("> ");
-                string choice = Console.ReadLine();
-                try
-                {
-                    selection = int.Parse(choice);
+                Console.Write ("> ");
+                string choice = Console.ReadLine ();
+                try {
+                    selection = int.Parse (choice);
+                } catch {
+                    Console.WriteLine ("Invalid Selection. Please try again.");
                 }
-                catch
-                {
-                    Console.WriteLine("Invalid Selection. Please try again.");
-                }
-                Console.WriteLine();
+                Console.WriteLine ();
             }
 
             return selection;
--- VendingMachine.cs
+++ VendingMachine.cs
@@ -1,81 +1,78 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
-namespace Vending
-{
-    public class VendingMachine
-    {
-        private List<Product> _products = new List<Product>();
+namespace Vending {
+    public class VendingMachine {
+        private List<Product> _products = new List<Product> ();
 
-        public VendingMachine()
-        {
-            _products.Add(new Product() { Id = 1, Name = "Snickers", Price = .75 });
-            _products.Add(new Product() { Id = 2, Name = "Pepsi", Price = 1.25 });
-            _products.Add(new Product() { Id = 3, Name = "Diamond Ring", Price = 2_400 });
-            _products.Add(new Product() { Id = 4, Name = "Tesla", Price = 85_000 });
-            _products.Add(new Product() { Id = 5, Name = "Hot Pocket", Price = 1.50 });
+        public VendingMachine () {
+            _products.Add (new Product () { Id = 1, Name = "Snickers", Price = .75 });
+            _products.Add (new Product () { Id = 2, Name = "Pepsi", Price = 1.25 });
+            _products.Add (new Product () { Id = 3, Name = "Diamond Ring", Price = 2_400 });
+            _products.Add (new Product () { Id = 4, Name = "Tesla", Price = 85_000 });
+            _products.Add (new Product () { Id = 5, Name = "Hot Pocket", Price = 1.50 });
         }
 
         // Add a new product to the Vending Machine (For stocking machine)
-        public void AddProduct(Product newProduct)
-        {
-            throw new NotImplementedException();
+        public void AddProduct (Product newProduct) {
+            _products.Add (newProduct);
         }
 
         // Remove a product from the Vending Machine (for purchasing a product)
-        public void RemoveProduct(Product productToRemove)
-        {
-            throw new NotImplementedException();
+        public void RemoveProduct (Product productToRemove) {
+            _products.Remove (productToRemove);
         }
 
         // Get all products ordered by price (lowest on top)
-        public List<Product> GetAll()
-        {
-            throw new NotImplementedException();
+        public List<Product> GetAll () {
+            List<Product> sortedByPrice = _products.OrderBy (p => p.Price).ToList ();
+            return sortedByPrice;
         }
 
         // Find a product by name. Results should be ordered by name)
-        public List<Product> SearchByName(string nameCriteria)
-        {
-            throw new NotImplementedException();
+        public List<Product> SearchByName (string nameCriteria) {
+            List<Product> foundProduct = _products.Where (p => p.Name == nameCriteria).ToList ();
+            return foundProduct;
         }
 
         // Find a product between a range or prices. Results should be ordered by price
-        public List<Product> SearchByPrice(double minPrice, double maxPrice)
-        {
-            throw new NotImplementedException();
+        public List<Product> SearchByPrice (double minPrice, double maxPrice) {
+            List<Product> sortedByPrice = _products.Where (p => p.Price < maxPrice && p.Price > minPrice).ToList ();
+            return sortedByPrice;
         }
 
         // Return a product with a given ID. Return null if not found.
-        public Product GetById(int id)
-        {
-            throw new NotImplementedException();
+        public Product GetById (int id) {
+            Product purchasedProduct = _products.FirstOrDefault (p => p.Id == id);
+            return purchasedProduct;
         }
 
         // Return the cheapest product or null if there are no products
-        public Product GetCheapest()
-        {
-            throw new NotImplementedException();
+        public Product GetCheapest () {
+            List<Product> sortedByPrice = _products.OrderBy (p => p.Price).ToList ();
+            Product minimum = sortedByPrice[0];
+            return minimum;
         }
 
         // Return the most expensive product or null if there are no products
-        public Product GetMostExpensive()
-        {
-            throw new NotImplementedException();
+        public Product GetMostExpensive () {
+            List<Product> sortedByPrice = _products.OrderByDescending (p => p.Price).ToList ();
+            Product minimum = sortedByPrice[0];
+            return minimum;
         }
 
         // Return all the product names in alphabetical ordere
-        public List<string> GetProductNames()
-        {
-            throw new NotImplementedException();
+        public List<string> GetProductNames () {
+            List<string> names = _products.Select (p => p.Name).ToList ();
+            List<string> sortedNames = names.OrderBy (n => n).ToList ();
+            return sortedNames;
         }
 
         // Property to represent the total of all the products' prices.
-        public double TotalValue
-        {
-            get
-            {
-                throw new NotImplementedException();
+        public double TotalValue {
+            get {
+                return _products.Sum (p => p.Price);
             }
         }
     }
